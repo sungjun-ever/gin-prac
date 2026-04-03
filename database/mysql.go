0febff6afa6b1d99c4556cca86215158def6f99a
sungjun-ever <zkfmak10@gmail.com>
2026-04-03 12:36:37 +0900

    add mysql conn

---
+++ database/mysql.go
@@ -0,0 +1,26 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func ConnectDB() *gorm.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("DB 연결 실패!")
+	}
+
+	return db
+}
