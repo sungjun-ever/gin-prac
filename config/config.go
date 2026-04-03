ebfd845c12c6074a3dbd3beb235e57fa9020cda7
sungjun-ever <zkfmak10@gmail.com>
2026-04-03 12:45:54 +0900

    config.go 수정 경로 이슈로 수정

--- config/config.go
+++ config/config.go
@@ -2,13 +2,28 @@ package config
 
 import (
 	"log"
+	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal(".env 로딩 실패")
+	wd, _ := os.Getwd()
+
+	curr := wd
+
+	for i := 0; i < 5; i++ {
+		envPath := filepath.Join(curr, ".env")
+		if _, err := os.Stat(envPath); err == nil {
+			// 찾았다면 로드하고 종료
+			err := godotenv.Load(envPath)
+			if err != nil {
+				log.Fatalf("Error loading .env file: %v", err)
+			}
+			log.Printf(".env loaded from: %s", envPath)
+			return
+		}
+		curr = filepath.Dir(curr)
 	}
 }
